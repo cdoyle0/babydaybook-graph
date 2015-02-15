f695e5c9fb2289afdc4946915fa0001ae7e04b1c
cdoyle <cdoyle@twitter.com>
2015-02-15 12:55:37 -0800

    add convert script, adjust readme, rename export dir

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # babydaybook-graph
 Create graphs for https://play.google.com/store/apps/details?id=com.drillyapps.babydaybook
+
+# Convert the backup db file to data files for the graphing. The converted fill will be
+# Saved in the data directory. This is designed to work with BSD shell utilities (OS X).
+
+* Make sure sqlite3 is in your path.
+* Save the backup file to the convert directory.
+
+Invoke using `db-convert.sh convert/BabyDaybook_exportdate.db`.
---
+++ db-convert.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+CONVERT_DIR=${BE_CONVERT:-convert}
+DEFAULT_CONVERT_FILE=$(date -j -v-1d "+BabyDaybook_%Y%m%d.db")
+CONVERT_FILE=${1:-$CONVERT_DIR/$DEFAULT_CONVERT_FILE}
+DATA_DIR=${BE_DATA_DIR:-data}
+DATA_FILE=$(date "+export-%Y-%m-%d.%H.%M.%S.csv")
+
+echo $DEFAULT_CONVERT_FILE
+echo $CONVERT_FILE
+# sqlite3 ~/BabyDaybook_20150214.db <<!
+# .headers on
+# .mode csv
+# .output $DATA_FILE
+
+# SELECT
+# 	_id AS id,
+# 	type,
+# 	datetime(start_millis/1000, 'unixepoch') AS start,
+# 	datetime(end_millis/1000, 'unixepoch') AS end,
+# 	(end_millis - start_millis)/1000 AS duration,
+# 	notes,
+# 	side,
+# 	volume,
+# 	temperature,
+# 	pee,
+# 	poo,
+# 	hair_wash
+# FROM daily_actions
+# ORDER BY start_millis;
+# !
